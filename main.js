f377a29851b7dabcf11e394cf36285f4dd2c2010
safi-0412 <omonjanovas@gmail.com>
2025-01-09 23:27:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const  username = +prompt("ismingizni kiriting")
+const  userlogin = +prompt("loginisni kiriting")
+const  userpassword = +prompt("parolisni kiriting")
+
+// console.log(mars)
+
+const mars = {
+    ism:username,
+    login:userlogin,
+    pasword:userpassword,
+}
+
+console.log(mars);
